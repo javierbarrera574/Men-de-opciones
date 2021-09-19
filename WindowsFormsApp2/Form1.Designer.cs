3cff3d588497c582d1d47ff62a07442c3b0751aa
javier barrera <fcbjavier12@hotmail.com>
2021-09-18 21:44:24 -0300

    Agregar archivos de proyecto.

---
+++ WindowsFormsApp2/Form1.Designer.cs
@@ -0,0 +1,133 @@
+﻿
+namespace WindowsFormsApp2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblResultado = new System.Windows.Forms.Label();
+            this.btCerrar = new System.Windows.Forms.Button();
+            this.txtOpcion = new System.Windows.Forms.TextBox();
+            this.btEleccion = new System.Windows.Forms.Button();
+            this.btLlamar = new System.Windows.Forms.Button();
+            this.checkedListBox1 = new System.Windows.Forms.CheckedListBox();
+            this.SuspendLayout();
+            // 
+            // lblResultado
+            // 
+            this.lblResultado.AutoSize = true;
+            this.lblResultado.BackColor = System.Drawing.Color.Red;
+            this.lblResultado.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblResultado.Location = new System.Drawing.Point(606, 174);
+            this.lblResultado.Name = "lblResultado";
+            this.lblResultado.Size = new System.Drawing.Size(0, 25);
+            this.lblResultado.TabIndex = 1;
+            this.lblResultado.TextAlign = System.Drawing.ContentAlignment.TopRight;
+            this.lblResultado.Visible = false;
+            // 
+            // btCerrar
+            // 
+            this.btCerrar.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btCerrar.ForeColor = System.Drawing.Color.Red;
+            this.btCerrar.Location = new System.Drawing.Point(441, 239);
+            this.btCerrar.Name = "btCerrar";
+            this.btCerrar.Size = new System.Drawing.Size(91, 34);
+            this.btCerrar.TabIndex = 2;
+            this.btCerrar.Text = "Cerrar";
+            this.btCerrar.UseVisualStyleBackColor = true;
+            this.btCerrar.Click += new System.EventHandler(this.btEleccion_Click);
+            // 
+            // txtOpcion
+            // 
+            this.txtOpcion.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtOpcion.Location = new System.Drawing.Point(306, 85);
+            this.txtOpcion.Name = "txtOpcion";
+            this.txtOpcion.Size = new System.Drawing.Size(152, 38);
+            this.txtOpcion.TabIndex = 3;
+            this.txtOpcion.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            this.txtOpcion.TextChanged += new System.EventHandler(this.txtOpcion_TextChanged);
+            // 
+            // btEleccion
+            // 
+            this.btEleccion.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btEleccion.Location = new System.Drawing.Point(213, 239);
+            this.btEleccion.Name = "btEleccion";
+            this.btEleccion.Size = new System.Drawing.Size(110, 34);
+            this.btEleccion.TabIndex = 6;
+            this.btEleccion.Text = "Elección";
+            this.btEleccion.UseVisualStyleBackColor = true;
+            this.btEleccion.Click += new System.EventHandler(this.btEleccion_Click_1);
+            // 
+            // btLlamar
+            // 
+            this.btLlamar.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btLlamar.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(192)))));
+            this.btLlamar.Location = new System.Drawing.Point(317, 341);
+            this.btLlamar.Name = "btLlamar";
+            this.btLlamar.Size = new System.Drawing.Size(110, 34);
+            this.btLlamar.TabIndex = 7;
+            this.btLlamar.Text = "Llamar";
+            this.btLlamar.UseVisualStyleBackColor = true;
+            // 
+            // checkedListBox1
+            // 
+            this.checkedListBox1.FormattingEnabled = true;
+            this.checkedListBox1.Location = new System.Drawing.Point(50, 74);
+            this.checkedListBox1.Name = "checkedListBox1";
+            this.checkedListBox1.Size = new System.Drawing.Size(150, 199);
+            this.checkedListBox1.TabIndex = 8;
+            this.checkedListBox1.SelectedIndexChanged += new System.EventHandler(this.checkedListBox1_SelectedIndexChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.checkedListBox1);
+            this.Controls.Add(this.btLlamar);
+            this.Controls.Add(this.btEleccion);
+            this.Controls.Add(this.txtOpcion);
+            this.Controls.Add(this.btCerrar);
+            this.Controls.Add(this.lblResultado);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label lblResultado;
+        private System.Windows.Forms.Button btCerrar;
+        private System.Windows.Forms.TextBox txtOpcion;
+        private System.Windows.Forms.Button btEleccion;
+        private System.Windows.Forms.Button btLlamar;
+        private System.Windows.Forms.CheckedListBox checkedListBox1;
+    }
+}
+
---
+++ WindowsFormsApp2/Form1.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp2
+{
+    public partial class Form1 : Form
+    {
+        int opcion = 0;
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            lblResultado.Text = "";
+        }
+
+        private void txtOpcion_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btEleccion_Click(object sender, EventArgs e)
+        {
+            this.Close();//usar esto para depurar el codigo de la calculadora
+        }
+
+     
+
+        private void btEleccion_Click_1(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void checkedListBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            //MENU DE OPCIONES
+
+
+            int opciones = 0;
+            
+            switch (opciones)
+            {
+                case 1:
+                    txtOpcion.Text = "1: automovil";
+                    lblResultado.Text = "Elegiste automovil";
+                    break;
+            }
+        }
+    }
+}
+
+
+//CONECTAR SI O SI A GitHub Y SUBIRLO AL REPOSITORIO
